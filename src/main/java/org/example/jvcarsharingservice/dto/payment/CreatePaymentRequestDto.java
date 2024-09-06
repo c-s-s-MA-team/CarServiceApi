fc0f6ccd2727e1d882fe9efc88ec150570d8472e
Banderan <bartoszwojcik2004@gmail.com>
2024-09-06 12:52:16 +0200

    add annotation

--- src/main/java/org/example/jvcarsharingservice/dto/payment/CreatePaymentRequestDto.java
+++ src/main/java/org/example/jvcarsharingservice/dto/payment/CreatePaymentRequestDto.java
@@ -1,6 +1,9 @@
 package org.example.jvcarsharingservice.dto.payment;
 
+import jakarta.validation.constraints.PositiveOrZero;
+
 public record CreatePaymentRequestDto(
+        @PositiveOrZero
         Long rentalId
 ) {
 }
--- src/main/java/org/example/jvcarsharingservice/dto/rental/CreateRentalRequestDto.java
+++ src/main/java/org/example/jvcarsharingservice/dto/rental/CreateRentalRequestDto.java
@@ -1,10 +1,15 @@
 package org.example.jvcarsharingservice.dto.rental;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.time.LocalDate;
 
 public record CreateRentalRequestDto(
+        @NotNull
         LocalDate rentalDate,
+        @NotNull
         LocalDate returnDate,
+        @PositiveOrZero
         Long carId
 ) {
 }
